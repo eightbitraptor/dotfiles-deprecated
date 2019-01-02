69eec44823cbdb10a9fb34fc707758352f915ae1
Matt Valentine-House <matt.valentinehouse@shopify.com>
2019-01-02 12:07:19 +0000

    Add some shortcuts to resize windows

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -238,6 +238,12 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
+;; resize windows
+(global-set-key (kbd "s-<left>")  'shrink-window-horizontally)
+(global-set-key (kbd "s-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "s-<up>")    'enlarge-window)
+(global-set-key (kbd "s-<down>")  'shrink-window)
+
 (global-set-key (kbd "s-<return>") 'toggle-frame-fullscreen)
 
 ;; Enable mouse support
