91bbeb9bfb8e268fb5a2f17bfb97a3ac2547d657
Matt Valentine-House <matt.valentinehouse@shopify.com>
2019-01-02 11:41:26 +0000

    Turn on line numbers globally
    Using the new mechanism in Emacs 26

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -196,6 +196,7 @@
 (setq initial-scratch-message "")
 (setq inhibit-startup-message t)
 (setq-default line-spacing 5)
+(global-display-line-numbers-mode t)
 (set-face-attribute 'default nil :font "Fira Code 14")
 
 ;; Save/Backup file behaviour
