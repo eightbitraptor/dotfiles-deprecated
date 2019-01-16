3c784f9b1a0178da0207fbe286ff2c9abc46e529
Matt Valentine-House <matt.valentinehouse@shopify.com>
2019-01-16 09:42:10 +0000

    Use fzf for finding files

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -135,7 +135,7 @@
   :config (ivy-rich-mode 1)
   :bind ("C-s"     . swiper)
         ("M-x"     . counsel-M-x)
-        ("C-x C-f" . counsel-find-file)
+        ("C-x C-f" . counsel-fzf)
         ("<f2> i"  . counsel-info-lookup-symbol)
         ("C-c u "  . counsel-unicode-char)
         ("C-c j"   . counsel-git-grep)
