e45a905da5420522bdb7b2dfd1af84af3018b20d
Matt Valentine-House <matt.valentinehouse@shopify.com>
2019-04-26 22:13:03 +0100

    Add JSX to the web mode alist

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -296,6 +296,7 @@
   :init (setq web-mode-markup-indent-offset 2)
         (setq web-mode-css-indent-offset 2)
         (setq web-mode-code-indent-offset 2)
+        (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
         (setq web-mode-enable-auto-indentation nil))
 
 
