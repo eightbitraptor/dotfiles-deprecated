078cf80a0b90fb50bf18a1118c476c4aee5f6458
Matt Valentine-House <matt@eightbitraptor.com>
2019-07-18 16:07:10 +0100

    Use a smaller font on Linux
    because my Linux boxes don't have HiDPI screens

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -54,7 +54,10 @@
 (setq inhibit-startup-message t)
 (setq-default line-spacing 5)
 (global-display-line-numbers-mode t)
-(set-face-attribute 'default nil :font "Fira Code 16")
+(cond
+ ((eq (window-system) 'x)
+  (set-face-attribute 'default nil :font "Fira Code 12"))
+ (set-face-attribute 'default nil :font "Fira Code 16"))
 
 ;;; General editor behaviour
 
