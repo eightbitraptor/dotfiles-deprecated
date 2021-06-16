306427193678c9ba34137751b6fbe8c24224cf5a
Matt Valentine-House <matt@eightbitraptor.com>
2021-06-16 11:31:51 +0100

    [emacs] Change the default font on Linux

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -55,7 +55,8 @@
 (setq inhibit-startup-message t)
 (setq-default line-spacing 5)
 (global-display-line-numbers-mode t)
-(cond ((eq (window-system) 'x)  (set-face-attribute 'default nil :font "Consolas 14"))
+
+(cond ((eq (window-system) 'x)  (set-face-attribute 'default nil :font "Liberation Mono 14"))
       ((eq (window-system) 'mac) (set-face-attribute 'default nil :font "Consolas 16")))
 
 ;;; General editor behaviour
